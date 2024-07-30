21e57b19b4faab5906ab7cf3202778040834662f
Daniel <wirelessdan@proton.me>
2024-07-30 01:58:39 +0000

    Working prototype

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+echo "███████╗██████╗ ███████╗██╗  ██╗████████╗██████╗ 
+██╔════╝██╔══██╗██╔════╝██║ ██╔╝╚══██╔══╝██╔══██╗
+███████╗██████╔╝█████╗  █████╔╝    ██║   ██████╔╝
+╚════██║██╔═══╝ ██╔══╝  ██╔═██╗    ██║   ██╔══██╗
+███████║██║     ███████╗██║  ██╗   ██║   ██║  ██║
+╚══════╝╚═╝     ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝  ╚═╝
+                                                 
+██████╗ ██╗   ██╗                                
+██╔══██╗╚██╗ ██╔╝                                
+██████╔╝ ╚████╔╝                                 
+██╔══██╗  ╚██╔╝                                  
+██████╔╝   ██║                                   
+╚═════╝    ╚═╝                                   
+                                                 
+███████╗██╗  ██╗██╗   ██╗██████╗ ██████╗         
+██╔════╝██║ ██╔╝██║   ██║██╔══██╗██╔══██╗        
+███████╗█████╔╝ ██║   ██║██████╔╝██████╔╝        
+╚════██║██╔═██╗ ██║   ██║██╔══██╗██╔══██╗        
+███████║██║  ██╗╚██████╔╝██║  ██║██║  ██║        
+╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝        
+                                                 "
+#copies to directory where cron can use. Change "cp" to "mv" to make this tidier
+cp ./spektr.py /usr/local/bin/
+
+#make it executable
+chmod +x /usr/local/bin/spektr.py
+
+read -p "What shall we call this machine?: " this_machine
+
+# Create the spektr_config.json file in /usr/local/bin/
+echo "{\"this machine\": \"$this_machine\", \"API\": \"milkfrogs.com\"}" > /usr/local/bin/spektr_config.json > /usr/local/bin/spektr_config.json
+
+# Give the file read permission to everyone
+chmod 755 /usr/local/bin/spektr_config.json
+
+# Add cron job
+(sudo crontab -l; echo "*/1 * * * * sudo python3 /usr/local/bin/spektr.py") | crontab -
---
+++ spektr.py
@@ -0,0 +1,16 @@
+import requests
+import json
+
+# Send the request and store the response
+response  = requests.get('https://api.ipify.org/')
+
+# Read the config.json file installed at /usr/local/bin
+with open('/usr/local/bin/spektr_config.json', 'r') as f:
+    config = json.load(f)
+
+# Access the value from the JSON object
+machine_name = config['this machine']
+
+# Save the content of the response as a JSON file with the new key-value pair
+with open('/usr/local/bin/public_ip.json', 'w') as f:
+    json.dump({'machine_name': machine_name, 'IP Address': response.text.strip()}, f)
