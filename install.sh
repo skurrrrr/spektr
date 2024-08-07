58a7c73d8aa4d84f61062cb1a0c59a9b0214541d
Daniel <wirelessdan@proton.me>
2024-08-07 04:27:32 +0000

    minor changes

--- install.sh
+++ install.sh
@@ -21,19 +21,17 @@ echo "███████╗██████╗ ███████╗█�
 ███████║██║  ██╗╚██████╔╝██║  ██║██║  ██║        
 ╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝        
                                                  "
-#copies to directory where cron can use. Change "cp" to "mv" to make this tidier
-cp ./spektr.py /usr/local/bin/
 
 #make it executable
-chmod +x /usr/local/bin/spektr.py
+chmod +x /usr/local/bin/spektr/spektr.py
 
 read -p "What shall we call this machine?: " this_machine
 
-# Create the spektr_config.json file in /usr/local/bin/
-echo "{\"this machine\": \"$this_machine\", \"API\": \"milfrogs.com\"}" > /usr/local/bin/spektr_config.json > /usr/local/bin/spektr_config.json
+# Create the spektr_config.json file in /usr/local/bin/spektr
+echo "{\"this machine\": \"$this_machine\", \"API\": \"milfrogs.com\"}" > /usr/local/bin/spektr/spektr_config.json > /usr/local/bin/spektr/spektr_config.json
 
 # Give the file read permission to everyone
-chmod 755 /usr/local/bin/spektr_config.json
+chmod 755 /usr/local/bin/spektr/spektr_config.json
 
 # Add cron job
-(sudo crontab -l; echo "*/1 * * * * sudo python3 /usr/local/bin/spektr.py") | crontab -
+(sudo crontab -l; echo "*/1 * * * * sudo python3 /usr/local/bin/spektr/spektr.py") | crontab -
