daba9cdd9bdbe65c28e3c509a63d0f81e0a4dbe9
Daniel <wirelessdan@proton.me>
2024-08-15 18:03:39 -0700

    I think I added Notify functionality.

--- spektr.py
+++ spektr.py
@@ -1,16 +1,22 @@
-import requests
 import json
+import requests
 
-# Send the request and store the response
-response  = requests.get('https://api.ipify.org/')
+with open('/usr/local/bin/spektr/ip.json') as f:
+    data = json.load(f)
 
-# Read the config.json file installed at /usr/local/bin
-with open('/usr/local/bin/spektr/spektr_config.json', 'r') as f:
-    config = json.load(f)
+webhook_url = 'https://discord.com/api/webhooks/1272102141690904586/4vqKTtmOVps4MfOEeGXidL4WcrrDYnaGMd9CPeQVblulzrAE9Go4r-qfCeqleQgYb_Xp'
+data = {
+    "username": data["machine_name"],
+    "content": f"IP Address: {data['IP Address']}"
+}
 
-# Access the value from the JSON object
-machine_name = config['this machine']
+headers = {
+    'Content-Type': 'application/json'
+}
 
-# Save the content of the response as a JSON file with the new key-value pair
-with open('/usr/local/bin/spektr/ip.json', 'w') as f:
-    json.dump({'machine_name': machine_name, 'IP Address': response.text.strip()}, f)
+response = requests.post(webhook_url, json=data, headers=headers)
+
+if response.status_code == 204:
+    print("Message sent successfully!")
+else:
+    print(f"Error: {response.text}")
